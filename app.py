5772498812342c2f9d43ce46b92c3804bd733c1f
Matthew <bmatthewtaylor@gmail.com>
2017-04-26 13:04:37 +1000

    part 2 completed, working

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, json, request
+from flask import Flask, render_template, json, request,redirect,session
 #from flask.ext.mysql import MySQL
 #from flask_mysql import MySQL
 from flaskext.mysql import MySQL
@@ -7,6 +7,8 @@ import sys
 
 mysql = MySQL()
 app = Flask(__name__)
+app.secret_key = 'why would I tell you my secret key?'
+#app.run(debug = True)  #restarts server when changes detected.
  
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'Matthew'
@@ -17,14 +19,63 @@ mysql.init_app(app)
 
 
 
-
 @app.route("/")
 def main():
     return render_template('index.html')
 
+@app.route('/userHome')
+def userHome():
+    if session.get('user'):
+        return render_template('userHome.html')
+    else:
+        return render_template('error.html',error = 'Unauthorized Access')
+
 @app.route('/showSignUp')
 def showSignUp():
     return render_template('signup.html')
+
+@app.route('/showSignin')
+def showSignin():
+	return render_template('signin.html')
+
+@app.route('/logout')
+def logout():
+	session.pop('user',None)
+	return redirect('/')#returns to route '/' above 
+
+
+@app.route('/validateLogin',methods=['POST'])
+def validateLogin():
+	try:
+		_username = request.form['inputEmail']
+		_password = request.form['inputPassword']
+ 
+		# connect to mysql
+ 
+		con = mysql.connect()
+		cursor = con.cursor()
+		cursor.callproc('sp_validateLogin',(_username,))
+		data = cursor.fetchall()
+ 
+		if len(data) > 0:
+			if check_password_hash(str(data[0][3]),_password):
+				print('check_password_hash = True', file=sys.stderr)
+				session['user'] = data[0][0]
+				return redirect('/userHome')
+			else:
+				print('check_password_hash = False', file=sys.stderr)
+				return render_template('error.html',error = 'Wrong Email address or Password.')
+		else:
+			print('len(data) !> 0', file=sys.stderr)
+			return render_template('error.html',error = 'Wrong Email address or Password.')
+ 
+ 
+	except Exception as e:
+		print('Exception = '+str(e), file=sys.stderr)
+		return render_template('error.html',error = str(e))
+	finally:
+		cursor.close()
+		con.close()
 
 
 @app.route('/signUp',methods=['POST'])
